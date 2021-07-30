9428a6c2140c7255b3fc9fa0ff47d220311a47a0
Dethree <87226621+Dethree@users.noreply.github.com>
2021-07-30 03:25:05 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
-pkg install python3
+pkg install python
+clear
+apt install python
+clear
 pip3 install pyrogram
+clear
 pip3 install tgcrypto
+clear
 pip3 install asyncio
+clear
 cd tg_inviter
 python3 -m run.py
